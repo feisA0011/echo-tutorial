78ad751a9958f5fbca333344b9cd7aa379df1dfe
Feisal <luffynation@gmail.com>
2025-12-10 23:49:38 +0100

    03.2: clerk authentication

--- apps/widget/app/page.tsx
+++ apps/widget/app/page.tsx
@@ -4,6 +4,7 @@ import {useMutation, useQuery} from "convex/react"
 import {api} from "@workspace/backend/_generated/api"
 import {Button} from "@workspace/ui/components/button"
 
+
 export default function Page() {
 const users= useQuery(api.users.getMany); 
 const addUser = useMutation(api.users.add)
@@ -11,12 +12,13 @@ const addUser = useMutation(api.users.add)
   return (
     <div className="flex flex-col items-center justify-center min-h-svh">
       
-        <p>wiggy</p>
+       
+        <p>serasera</p>
         <Button onClick={()=>addUser()}>Add</Button>
         <div className="max-w-sm w-full mx-auto">{
           JSON.stringify(users, null, 2)}
         </div>
-
+       
     </div>
   )
 }
