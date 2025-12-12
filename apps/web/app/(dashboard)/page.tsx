7a8392a824495b194a2b29a3c745ef26beceae1e
Feisal <luffynation@gmail.com>
2025-12-13 00:28:47 +0100

    04: organizations

--- apps/web/app/(dashboard)/page.tsx
+++ apps/web/app/(dashboard)/page.tsx
@@ -1,29 +1,23 @@
 "use client";
-import { SignInButton, UserButton } from "@clerk/nextjs";
-import {useMutation, useQuery, Authenticated, Unauthenticated} from "convex/react" 
+import { OrganizationSwitcher, UserButton } from "@clerk/nextjs";
+import {useMutation, useQuery} from "convex/react" 
 import {api} from "@workspace/backend/_generated/api"
 import {Button} from "@workspace/ui/components/button"
+
 
 export default function Page() {
 const users= useQuery(api.users.getMany); 
 const addUser =useMutation(api.users.add)
 
-  return (
-    <>
-      <Authenticated>
+  return (  
         <div className="flex flex-col items-center justify-center min-h-svh">
           <p>Luffy</p>
           <UserButton />
+          <OrganizationSwitcher hidePersonal />
           <Button onClick={()=>addUser()}>Add</Button>
           <div className="max-w-sm w-full mx-auto">{
             JSON.stringify(users, null, 2)}
           </div>
         </div>
-      </Authenticated>
-      <Unauthenticated>
-        <p>"Please sign in to view this page!."</p> 
-        <SignInButton >Sign In!</SignInButton>
-      </Unauthenticated>
-    </>
   )
 }
---
+++ apps/web/modules/auth/ui/views/org-select-view.tsx
@@ -0,0 +1,14 @@
+import { OrganizationList } from "@clerk/nextjs";
+
+
+export const OrgSelectView = () => {
+    return(
+        <OrganizationList 
+        afterCreateOrganizationUrl="/"
+        afterSelectOrganizationUrl="/"
+        hidePersonal
+        skipInvitationScreen
+        />
+    )
+    }
+    
